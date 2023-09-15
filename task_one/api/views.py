9f4416d6a434fb721ca9c955ab0dd28d6824da2d
Madi <madi_gaziz@mail.ru>
2023-09-16 03:15:55 +0600

    Обновление кода задания №1: Удалил не нужные импорты, удалил не нужный print, добавил дополнительное условие если метод не post

--- task_one/api/views.py
+++ task_one/api/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-from datetime import datetime
-from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+from django.views.decorators.csrf import ensure_csrf_cookie
 import json
 # Create your views here.
 
@@ -13,20 +11,21 @@ def calculate(request, operation):
             data = json.loads(request.body)
             if 'A' in data and 'B' in data:
                 a  = data['A']
-                b = data['B']
-                print(True)
-            if operation == 'add':
-                result = a + b
-            elif operation == 'subtract':
-                result = a - b
-            elif operation == 'multiply':
-                result = a * b
-            elif operation == 'divide':
-                if b == 0:
-                    return JsonResponse({'error': 'Division by zero'}, status=400)
-                result = a / b
-            else:
-                return JsonResponse({'error': 'Invalid operation'}, status=400)
-            return JsonResponse({'answer': result})
-        except:
+                b =  data['B']
+                if operation == 'add':
+                    result = a + b
+                elif operation == 'subtract':
+                    result = a - b
+                elif operation == 'multiply':
+                    result = a * b
+                elif operation == 'divide':
+                    if b == 0:
+                        return JsonResponse({'error': 'Division by zero'}, status=400)
+                    result = a / b
+                else:
+                    return JsonResponse({'error': 'Invalid operation'}, status=400)
+                return JsonResponse({'answer': result})
+        except ValueError:
             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
