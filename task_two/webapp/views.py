b1f722363d67ac814f39d97f5a4c9d05cb683050
Madi <madi_gaziz@mail.ru>
2023-09-17 00:02:37 +0600

    Задание №2: Создал класс CalculatorView для показа страницы index.html, а также перенес функции calculate для работоспособности api запросов

--- task_two/webapp/views.py
+++ task_two/webapp/views.py
@@ -1,3 +1,39 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class CalculatorView(TemplateView):
+    template_name = 'index.html'
+
+    def calculate(request, operation):
+        if request.method == 'POST':
+            try:
+                data = json.loads(request.body)
+                if 'A' in data and 'B' in data:
+                    a = data['A']
+                    b = data['B']
+                    if operation == 'add':
+                        result = a + b
+                    elif operation == 'subtract':
+                        result = a - b
+                    elif operation == 'multiply':
+                        result = a * b
+                    elif operation == 'divide':
+                        if b == 0:
+                            return JsonResponse({'error': 'Division by zero'}, status=400)
+                        result = a / b
+                    else:
+                        return JsonResponse({'error': 'Invalid operation'}, status=400)
+                    print('work')
+                    print(result)
+                    return JsonResponse({'result': result})
+            except ValueError:
+                return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+        else:
+            return JsonResponse({'error': 'Method not allowed'}, status=405)
+
